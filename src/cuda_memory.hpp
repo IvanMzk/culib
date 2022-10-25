cf4711c6bbd52c054c10e8ae2937a5c48cff06cb
Ivan Malezhyk <ivanmzk@gmail.com>
2022-10-25 14:16:24 +0300

    refactor basic_pointer make it base for more specific pointer type using CRTP

--- src/cuda_memory.hpp
+++ src/cuda_memory.hpp
@@ -4,58 +4,77 @@
 #include <iterator>
 #include <memory>
 #include "cuda_helpers.hpp"
-#include "thrust\device_vector.h"
 
 namespace cuda_experimental{
 
-template<typename T>
+template<typename T, template<typename> typename D>
 class basic_pointer{
+    using derived_type = D<T>;
 public:
     using value_type = T;
     using pointer = T*;
-
-    basic_pointer(const basic_pointer&) = default;
     basic_pointer& operator=(const basic_pointer&) = default;
-    basic_pointer(pointer ptr_ = nullptr):
-        ptr{ptr_}
-    {}
-    basic_pointer& operator=(std::nullptr_t){
+    basic_pointer& operator=(basic_pointer&&) = default;
+    derived_type& operator=(std::nullptr_t){
         ptr = nullptr;
-        return *this;
+        return static_cast<derived_type&>(*this);
     }
     operator bool()const{return static_cast<bool>(ptr);}
-    operator basic_pointer<const T>()const{return basic_pointer<const T>{ptr};}
+    operator D<const T>()const{return D<const T>{ptr};}
     pointer get()const{return ptr;}
 private:
+    friend derived_type;
+    basic_pointer(const basic_pointer&) = default;
+    basic_pointer(basic_pointer&&) = default;
+    explicit basic_pointer(pointer ptr_ = nullptr):
+        ptr{ptr_}
+    {}
     pointer ptr;
 };
 
 template<typename T>
-auto operator==(const basic_pointer<T>& lhs, const basic_pointer<T>& rhs){return lhs.get() == rhs.get();}
-template<typename T>
-auto operator!=(const basic_pointer<T>& lhs, const basic_pointer<T>& rhs){return !(lhs == rhs);}
-template<typename T>
-auto operator-(const basic_pointer<T>& lhs, const basic_pointer<T>& rhs){return lhs.get() - rhs.get();}
-template<typename T, typename U>
-auto operator+(const basic_pointer<T>& lhs, const U& rhs){return basic_pointer<T>{lhs.get() + rhs};}
-template<typename T, typename U>
-auto operator+(const U& lhs, const basic_pointer<T>& rhs){return rhs+lhs;}
-template<typename T, typename U>
-auto operator-(const basic_pointer<T>& lhs, const U& rhs){return lhs+-rhs;}
+class is_basic_pointer_t{
+    template<typename...U>
+    static std::true_type selector(const basic_pointer<U...>&);
+    static std::false_type selector(...);
+public: using type = decltype(selector(std::declval<T>()));
+};
+template<typename T> constexpr bool is_basic_pointer_v = is_basic_pointer_t<T>::type();
 
-template<typename T>
-auto distance(const basic_pointer<T>& begin, const basic_pointer<T>& end){return end-begin;}
+template<typename T, template<typename> typename D, typename U, std::enable_if_t<!is_basic_pointer_v<U>,int> =0>
+auto operator+(const basic_pointer<T,D>& lhs, const U& rhs){return D<T>{lhs.get() + rhs};}
+template<typename T, template<typename> typename D, typename U, std::enable_if_t<!is_basic_pointer_v<U>,int> =0>
+auto operator+(const U& lhs, const basic_pointer<T,D>& rhs){return rhs+lhs;}
+template<typename T, template<typename> typename D, typename U, std::enable_if_t<!is_basic_pointer_v<U>,int> =0 >
+auto operator-(const basic_pointer<T,D>& lhs, const U& rhs){return lhs+-rhs;}
 
-template<typename T>
-auto ptr_to_void(const basic_pointer<T>& p){return static_cast<std::conditional_t<std::is_const_v<T>,const void*,void*>>(p.get());}
+template<typename T, template<typename> typename D>
+auto operator==(const basic_pointer<T,D>& lhs, const basic_pointer<T,D>& rhs){return lhs.get() == rhs.get();}
+template<typename T, template<typename> typename D>
+auto operator!=(const basic_pointer<T,D>& lhs, const basic_pointer<T,D>& rhs){return !(lhs == rhs);}
+template<typename T, template<typename> typename D>
+auto operator-(const basic_pointer<T,D>& lhs, const basic_pointer<T,D>& rhs){return lhs.get() - rhs.get();}
+template<typename T, template<typename> typename D>
+auto distance(const basic_pointer<T,D>& begin, const basic_pointer<T,D>& end){return end-begin;}
+template<typename T, template<typename> typename D>
+auto ptr_to_void(const basic_pointer<T,D>& p){return static_cast<std::conditional_t<std::is_const_v<T>,const void*,void*>>(p.get());}
 template<typename T>
 auto ptr_to_void(const T* p){return static_cast<const void*>(p);}
 template<typename T>
 auto ptr_to_void(T* p){return static_cast<void*>(p);}
 
-template<typename> constexpr bool is_basic_pointer_v = false;
-template<typename T> constexpr bool is_basic_pointer_v<basic_pointer<T>> = true;
-
+template<typename T>
+class device_pointer : public basic_pointer<T,device_pointer>
+{
+public:
+    using typename basic_pointer::value_type;
+    using typename basic_pointer::pointer;
+    device_pointer(pointer p= nullptr):
+        basic_pointer{p}
+    {}
+    using basic_pointer::operator=;
+    auto operator*();
+};
 
 
 template<typename T>
@@ -65,11 +84,11 @@ public:
     using difference_type = std::ptrdiff_t;
     using size_type = difference_type;
     using value_type = T;
-    using pointer = basic_pointer<T>;
-    using const_pointer = basic_pointer<const T>;
+    using pointer = device_pointer<T>;
+    using const_pointer = device_pointer<const T>;
 
     pointer allocate(size_type n){
-        return void* p;
+        void* p;
         cuda_error_check(cudaMalloc(&p,n*sizeof(T)));
         return pointer{static_cast<T*>(p)};
     }
@@ -79,118 +98,119 @@ public:
     bool operator==(const device_allocator& other)const{return true;}
 };
 
-template<typename T>
-class cuda_allocator
-{
-    using device_id_type = int;
-public:
-    using difference_type = std::ptrdiff_t;
-    using size_type = difference_type;
-    using value_type = T;
-    using pointer = basic_pointer<T>;
-    using const_pointer = basic_pointer<const T>;
-    using propagate_on_container_copy_assignment = std::true_type;
-    using propagate_on_container_move_assignment = std::true_type;
 
-    cuda_allocator(const cuda_allocator&) = default;
-    cuda_allocator(cuda_allocator&&) = default;
-    cuda_allocator& operator=(const cuda_allocator&) = default;
-    cuda_allocator& operator=(cuda_allocator&&) = default;
-    cuda_allocator(device_id_type managed_device_id_ = cuda_get_device()):
-        managed_device_id{managed_device_id_}
-    {}
-    pointer allocate(size_type n){
-        return allocator_helper([this](size_type n){void* p; cuda_error_check(cudaMalloc(&p,n*sizeof(T))); return pointer{static_cast<T*>(p)};}, n);
-    }
-    void deallocate(pointer p, size_type){
-        return allocator_helper([this](pointer p){cuda_error_check(cudaFree(ptr_to_void(p)));}, p);
-    }
-    bool operator==(const cuda_allocator& other)const{return managed_device_id == other.managed_device_id;}
-private:
-    struct device_restorer{
-        ~device_restorer(){cuda_error_check(cudaSetDevice(device));}
-        device_id_type device{cuda_get_device()};
-    };
-    template<typename F, typename...Args>
-    auto allocator_helper(const F& operation, Args&&...args){
-        device_restorer restorer{};
-        cuda_error_check(cudaSetDevice(managed_device_id));
-        return operation(std::forward<Args>(args)...);
-    }
+// template<typename T>
+// class cuda_allocator
+// {
+//     using device_id_type = int;
+// public:
+//     using difference_type = std::ptrdiff_t;
+//     using size_type = difference_type;
+//     using value_type = T;
+//     using pointer = basic_pointer<T>;
+//     using const_pointer = basic_pointer<const T>;
+//     using propagate_on_container_copy_assignment = std::true_type;
+//     using propagate_on_container_move_assignment = std::true_type;
 
-    device_id_type managed_device_id;
-};
+//     cuda_allocator(const cuda_allocator&) = default;
+//     cuda_allocator(cuda_allocator&&) = default;
+//     cuda_allocator& operator=(const cuda_allocator&) = default;
+//     cuda_allocator& operator=(cuda_allocator&&) = default;
+//     cuda_allocator(device_id_type managed_device_id_ = cuda_get_device()):
+//         managed_device_id{managed_device_id_}
+//     {}
+//     pointer allocate(size_type n){
+//         return allocator_helper([this](size_type n){void* p; cuda_error_check(cudaMalloc(&p,n*sizeof(T))); return pointer{static_cast<T*>(p)};}, n);
+//     }
+//     void deallocate(pointer p, size_type){
+//         return allocator_helper([this](pointer p){cuda_error_check(cudaFree(ptr_to_void(p)));}, p);
+//     }
+//     bool operator==(const cuda_allocator& other)const{return managed_device_id == other.managed_device_id;}
+// private:
+//     struct device_restorer{
+//         ~device_restorer(){cuda_error_check(cudaSetDevice(device));}
+//         device_id_type device{cuda_get_device()};
+//     };
+//     template<typename F, typename...Args>
+//     auto allocator_helper(const F& operation, Args&&...args){
+//         device_restorer restorer{};
+//         cuda_error_check(cudaSetDevice(managed_device_id));
+//         return operation(std::forward<Args>(args)...);
+//     }
 
-template<typename T>
-class unified_memory_allocator
-{
-public:
-    using difference_type = std::ptrdiff_t;
-    using size_type = difference_type;
-    using value_type = T;
-    using pointer = basic_pointer<T>;
-    using const_pointer = basic_pointer<const T>;
+//     device_id_type managed_device_id;
+// };
 
-    pointer allocate(size_type n){
-        void* p;
-        cuda_error_check(cudaMallocManaged(&p,n*sizeof(T)));
-        return pointer{static_cast<T*>(p)};
-    }
-    void deallocate(pointer p, size_type){
-        cuda_error_check(cudaFree(ptr_to_void(p)));
-    }
-    bool operator==(const unified_memory_allocator& other)const{return true;}
-};
+// template<typename T>
+// class unified_memory_allocator
+// {
+// public:
+//     using difference_type = std::ptrdiff_t;
+//     using size_type = difference_type;
+//     using value_type = T;
+//     using pointer = basic_pointer<T>;
+//     using const_pointer = basic_pointer<const T>;
 
-/*
-* host memory allocator for cuda unified addressing
-* allocate host memory or use already allocated host memory
-*/
-template<typename T>
-class cuda_mapping_allocator
-{
-public:
-    using difference_type = std::ptrdiff_t;
-    using size_type = difference_type;
-    using value_type = T;
-    using pointer = basic_pointer<T>;
-    using const_pointer = basic_pointer<const T>;
-    using propagate_on_container_copy_assignment = std::true_type;
-    using propagate_on_container_move_assignment = std::true_type;
+//     pointer allocate(size_type n){
+//         void* p;
+//         cuda_error_check(cudaMallocManaged(&p,n*sizeof(T)));
+//         return pointer{static_cast<T*>(p)};
+//     }
+//     void deallocate(pointer p, size_type){
+//         cuda_error_check(cudaFree(ptr_to_void(p)));
+//     }
+//     bool operator==(const unified_memory_allocator& other)const{return true;}
+// };
 
-    cuda_mapping_allocator(const cuda_mapping_allocator&) = default;
-    cuda_mapping_allocator(cuda_mapping_allocator&&) = default;
-    cuda_mapping_allocator& operator=(const cuda_mapping_allocator&) = default;
-    cuda_mapping_allocator& operator=(cuda_mapping_allocator&&) = default;
-    cuda_mapping_allocator(T* host_data_ = nullptr):
-        host_data{host_data_}
-    {}
+// /*
+// * host memory allocator for cuda unified addressing
+// * allocate host memory or use already allocated host memory
+// */
+// template<typename T>
+// class cuda_mapping_allocator
+// {
+// public:
+//     using difference_type = std::ptrdiff_t;
+//     using size_type = difference_type;
+//     using value_type = T;
+//     using pointer = basic_pointer<T>;
+//     using const_pointer = basic_pointer<const T>;
+//     using propagate_on_container_copy_assignment = std::true_type;
+//     using propagate_on_container_move_assignment = std::true_type;
 
-    pointer allocate(size_type n){
-        void* host_buffer;
-        if (host_data){
-            host_buffer = host_data;
-            //cudaHostRegisterDefault: On a system with unified virtual addressing, the memory will be both mapped and portable.
-            //On a system with no unified virtual addressing, the memory will be neither mapped nor portable.
-            cuda_error_check(cudaHostRegister(host_buffer,n*sizeof(T),cudaHostRegisterDefault));
-        }else{
-            host_buffer = make_host_locked_buffer<value_type>(n).release();
-        }
-        void* p;
-        cuda_error_check(cudaHostGetDevicePointer(&p, host_buffer, 0));
-        return pointer{static_cast<T*>(p)};
-    }
-    void deallocate(pointer p, size_type){
-        if (host_data){
-            cuda_error_check(cudaHostUnregister(host_data));
-        }else{
-            cuda_error_check(cudaFreeHost(ptr_to_void(p)));
-        }
-    }
-    bool operator==(const cuda_mapping_allocator& other)const{return host_data == other.host_data;}
-private:
-    T* host_data;
-};
+//     cuda_mapping_allocator(const cuda_mapping_allocator&) = default;
+//     cuda_mapping_allocator(cuda_mapping_allocator&&) = default;
+//     cuda_mapping_allocator& operator=(const cuda_mapping_allocator&) = default;
+//     cuda_mapping_allocator& operator=(cuda_mapping_allocator&&) = default;
+//     cuda_mapping_allocator(T* host_data_ = nullptr):
+//         host_data{host_data_}
+//     {}
+
+//     pointer allocate(size_type n){
+//         void* host_buffer;
+//         if (host_data){
+//             host_buffer = host_data;
+//             //cudaHostRegisterDefault: On a system with unified virtual addressing, the memory will be both mapped and portable.
+//             //On a system with no unified virtual addressing, the memory will be neither mapped nor portable.
+//             cuda_error_check(cudaHostRegister(host_buffer,n*sizeof(T),cudaHostRegisterDefault));
+//         }else{
+//             host_buffer = make_host_locked_buffer<value_type>(n).release();
+//         }
+//         void* p;
+//         cuda_error_check(cudaHostGetDevicePointer(&p, host_buffer, 0));
+//         return pointer{static_cast<T*>(p)};
+//     }
+//     void deallocate(pointer p, size_type){
+//         if (host_data){
+//             cuda_error_check(cudaHostUnregister(host_data));
+//         }else{
+//             cuda_error_check(cudaFreeHost(ptr_to_void(p)));
+//         }
+//     }
+//     bool operator==(const cuda_mapping_allocator& other)const{return host_data == other.host_data;}
+// private:
+//     T* host_data;
+// };
 
 template<typename T, typename SizeT>
 auto make_host_locked_buffer(const SizeT& n, unsigned int flags = cudaHostAllocDefault){
@@ -208,12 +228,12 @@ auto make_host_buffer(const SizeT& n){
 //copy routines to transfer between host and device, parameters of ordinary pointers types treats as pointers to host memory
 //copy from host to device
 template<typename T>
-void copy(const T* first, const T* last, basic_pointer<T> d_first){
+void copy(const T* first, const T* last, device_pointer<T> d_first){
     auto n = std::distance(first,last);
     cuda_error_check(cudaMemcpy(ptr_to_void(d_first), ptr_to_void(first), n*sizeof(T), cudaMemcpyKind::cudaMemcpyHostToDevice));
 }
 template<typename It, std::enable_if_t<!std::is_pointer_v<It> && !is_basic_pointer_v<It>,int> =0 >
-void copy(It first, It last, basic_pointer<typename std::iterator_traits<It>::value_type> d_first){
+void copy(It first, It last, device_pointer<typename std::iterator_traits<It>::value_type> d_first){
     static_assert(!std::is_pointer_v<It>);
     auto n = std::distance(first,last);
     auto buffer = make_host_locked_buffer<std::iterator_traits<It>::value_type>(n,cudaHostAllocWriteCombined);
@@ -222,12 +242,12 @@ void copy(It first, It last, basic_pointer<typename std::iterator_traits<It>::va
 }
 //copy from device to host
 template<typename T>
-void copy(basic_pointer<T> first, basic_pointer<T> last, std::remove_const_t<T>* d_first){
+void copy(device_pointer<T> first, device_pointer<T> last, std::remove_const_t<T>* d_first){
     auto n = distance(first,last);
     cuda_error_check(cudaMemcpy(ptr_to_void(d_first), ptr_to_void(first), n*sizeof(T), cudaMemcpyKind::cudaMemcpyDeviceToHost));
 }
 template<typename T, typename It, std::enable_if_t<!std::is_pointer_v<It> && !is_basic_pointer_v<It>,int> =0>
-void copy(basic_pointer<T> first, basic_pointer<T> last, It d_first){
+void copy(device_pointer<T> first, device_pointer<T> last, It d_first){
     static_assert(!std::is_pointer_v<It>);
     static_assert(std::is_same_v<std::decay_t<T>, typename std::iterator_traits<It>::value_type>);
     auto n = distance(first,last);
@@ -237,7 +257,7 @@ void copy(basic_pointer<T> first, basic_pointer<T> last, It d_first){
 }
 //copy from device to device, src and dst must be allocated on same device
 template<typename T>
-void copy(basic_pointer<T> first, basic_pointer<T> last, basic_pointer<std::remove_const_t<T>> d_first){
+void copy(device_pointer<T> first, device_pointer<T> last, device_pointer<std::remove_const_t<T>> d_first){
     // if (first.device() != last.device()){
     //     throw cuda_exception("copy device-device invalid source range");
     // }
--- test/test_cuda_memory.cpp
+++ test/test_cuda_memory.cpp
@@ -2,42 +2,118 @@
 #include "catch.hpp"
 #include "cuda_memory.hpp"
 
+namespace test_cuda_memory{
+using cuda_experimental::basic_pointer;
+template<typename T>
+class test_pointer : public basic_pointer<T, test_pointer>
+{
+public:
+    using typename basic_pointer::pointer;
+    using typename basic_pointer::value_type;
+    using basic_pointer::operator=;
+    test_pointer(pointer p = nullptr):
+        basic_pointer(p)
+    {}
+};
+
+
+template<typename D>
+class A
+{
+public:
+    A& operator=(const A&) = default;
+    A& operator=(A&&) = default;
+    D& operator=(std::nullptr_t){
+        a = 0;
+        return static_cast<D&>(*this);
+    }
+    D& operator=(int a_){
+        a = a_;
+        return static_cast<D&>(*this);
+
+    }
+    auto get(){return a;}
+private:
+    friend D;
+    A(const A&) = default;
+    A(A&&) = default;
+    A(int a_ = 0):
+        a{a_}
+    {}
+    int a;
+};
+
+class B : public A<B>
+{
+public:
+    B(int b = 0) :
+        A(b)
+    {}
+    using A::operator=;
+};
+
+}   //end of namespace test_cuda_memory
 
 TEMPLATE_TEST_CASE("test_basic_pointer","[test_cuda_memory]",
-    cuda_experimental::basic_pointer<float>,
-    cuda_experimental::basic_pointer<const float>
+    test_cuda_memory::test_pointer<float>,
+    test_cuda_memory::test_pointer<const float>,
+    cuda_experimental::device_pointer<float>,
+    cuda_experimental::device_pointer<const float>
 )
 {
-    using value_type = float;
-    using basic_pointer_type = TestType;
 
-    REQUIRE(std::is_trivially_copyable_v<basic_pointer_type>);
-    float v{};
-    auto p = basic_pointer_type{};
+    SECTION("test_AB"){
+        using test_cuda_memory::A;
+        using test_cuda_memory::B;
+        REQUIRE(std::is_trivially_copyable_v<A<B>>);
+        REQUIRE(std::is_trivially_copyable_v<B>);
+        B b{1};
+        B b_{0};
+        B bb{};
+        REQUIRE(std::is_same_v<decltype(b_=b),B&>);
+        b_ = b;
+        REQUIRE(b_.get() == 1);
+        REQUIRE(b.get() == 1);
+
+        REQUIRE(std::is_same_v<decltype(b=nullptr),B&>);
+        b = nullptr;
+        REQUIRE(b.get() == 0);
+
+        REQUIRE(std::is_same_v<decltype(b=1),B&>);
+        b = 1;
+        REQUIRE(b.get() == 1);
+    }
+
+    using value_type = float;
+    using pointer_type = TestType;
+
+    REQUIRE(std::is_trivially_copyable_v<pointer_type>);
+    value_type v{};
+    auto p = pointer_type{};
     REQUIRE(p.get() == nullptr);
     SECTION("from_pointer_construction"){
-        auto p = basic_pointer_type{&v};
+        auto p = pointer_type{&v};
         REQUIRE(p.get() == &v);
-        auto p1 = basic_pointer_type{nullptr};
+        auto p1 = pointer_type{nullptr};
         REQUIRE(p1.get() == nullptr);
     }
     SECTION("copy_construction"){
-        auto p = basic_pointer_type{&v};
+        auto p = pointer_type{&v};
         auto p1 = p;
         REQUIRE(p1.get() == p.get());
     }
     SECTION("copy_assignment"){
-        auto p = basic_pointer_type{};
-        auto p1 = basic_pointer_type{&v};
+        auto p = pointer_type{};
+        auto p1 = pointer_type{&v};
         p = p1;
         REQUIRE(p.get() == p1.get());
         p1 = nullptr;
         REQUIRE(p1.get() == nullptr);
     }
     SECTION("equality"){
-        auto p = basic_pointer_type{};
-        auto p1 = basic_pointer_type{&v};
-        auto p2 = basic_pointer_type{&v};
+        auto p = pointer_type{};
+        auto p1 = pointer_type{&v};
+        auto p2 = pointer_type{&v};
         REQUIRE(p == p);
         REQUIRE(p1 == p1);
         REQUIRE(p1 == p2);
@@ -45,14 +121,16 @@ TEMPLATE_TEST_CASE("test_basic_pointer","[test_cuda_memory]",
     }
     SECTION("add_offset"){
         value_type a[10];
-        auto p = basic_pointer_type{a};
-        REQUIRE(p+5 == basic_pointer_type{a+5});
-        REQUIRE(5+p == basic_pointer_type{a+5});
+        auto p = pointer_type{a};
+        REQUIRE(std::is_same_v<decltype(p+5),pointer_type>);
+        REQUIRE(std::is_same_v<decltype(5+p),pointer_type>);
+        REQUIRE(p+5 == pointer_type{a+5});
+        REQUIRE(5+p == pointer_type{a+5});
     }
     SECTION("subtract_two_pointers"){
         value_type a[10];
-        auto begin = basic_pointer_type{a};
-        auto end = basic_pointer_type{a+10};
+        auto begin = pointer_type{a};
+        auto end = pointer_type{a+10};
         REQUIRE(end-begin == 10);
         REQUIRE(end-end == 0);
         REQUIRE(distance(begin,end) == 10);
@@ -61,25 +139,37 @@ TEMPLATE_TEST_CASE("test_basic_pointer","[test_cuda_memory]",
     }
     SECTION("distance"){
         value_type a[10];
-        auto begin = basic_pointer_type{a};
-        auto end = basic_pointer_type{a+10};
+        auto begin = pointer_type{a};
+        auto end = pointer_type{a+10};
         REQUIRE(distance(begin,end) == 10);
         REQUIRE(distance(begin,begin) == 0);
         REQUIRE(distance(begin,begin+3) == 3);
         REQUIRE(distance(begin+2,end-3) == 5);
     }
+    SECTION("bool_cast_operator"){
+        pointer_type p{};
+        REQUIRE(!static_cast<bool>(p));
+        pointer_type p1{&v};
+        REQUIRE(static_cast<bool>(p1));
+    }
+    SECTION("cast_to_pointer_to_const_operator"){
+        using test_cuda_memory::test_pointer;
+        test_pointer<value_type> p{&v};
+        auto cp = static_cast<test_pointer<const value_type>>(p);
+        REQUIRE(p == cp);
+    }
 }
 
 TEST_CASE("test_copy","[test_cuda_memory]"){
     using value_type = float;
-    using cuda_allocator_type = cuda_experimental::cuda_allocator<value_type>;
-    using cuda_experimental::basic_pointer;
+    using cuda_experimental::device_allocator;
+    using cuda_experimental::device_pointer;
     using cuda_experimental::copy;
 
-    auto allocator = cuda_allocator_type{};
+    auto allocator = device_allocator<value_type>{};
     constexpr std::size_t n{100};
     auto dev_ptr = allocator.allocate(n);
-    auto const_dev_ptr = basic_pointer<const value_type>{dev_ptr};
+    auto const_dev_ptr = device_pointer<const value_type>{dev_ptr};
 
     SECTION("copy_host_device"){
         constexpr std::size_t a_len{10};
